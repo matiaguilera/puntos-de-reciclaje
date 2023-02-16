8f1e7f95933f3bc530b44f1cf8d0f95e8e806413
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2023-02-16 13:55:33 -0300

    Agrega variables para deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'puntos-de-reciclaje-88753',
+    appId: '1:281014797212:web:83c0adadf42f01c34efe59',
+    storageBucket: 'puntos-de-reciclaje-88753.appspot.com',
+    apiKey: 'AIzaSyAUOO2QD9VFHI5xt5AbobOr7lqYi4oiyww',
+    authDomain: 'puntos-de-reciclaje-88753.firebaseapp.com',
+    messagingSenderId: '281014797212',
+  },
+};
