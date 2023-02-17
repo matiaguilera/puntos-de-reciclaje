4ea4c8d3832d220e7a508e6cf1abcade9c25b723
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2023-02-17 16:00:27 -0300

    Agrega interfaz básica con Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,130 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import * as mapboxgl from 'mapbox-gl';
+import booleanDisjoint from '@turf/boolean-disjoint';
+import lineIntersect from '@turf/line-intersect';
+import buffer from '@turf/buffer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Puntos de Reciclaje';
+export class AppComponent implements OnInit {
+  recyclingPoints = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-54.6541, -25.4938],
+        },
+        properties: {},
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-54.6527, -25.4903],
+        },
+        properties: {},
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-54.6558, -25.4871],
+        },
+        properties: {},
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-54.6603, -25.486],
+        },
+        properties: {},
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-54.6684, -25.485],
+        },
+        properties: {},
+      },
+    ],
+  };
+  mapbox = mapboxgl as typeof mapboxgl;
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/light-v11';
+  lat = -25.4938;
+  lng = -54.6541;
+  zoom = 15;
+  async getDirections(
+    originPoint: number[],
+    destinyPoint: number[],
+    accessToken: string
+  ) {
+    try {
+      let response = await fetch(
+        `https://api.mapbox.com/directions/v5/mapbox/driving/${originPoint[0]},${originPoint[1]};${destinyPoint[0]},${destinyPoint[1]}?geometries=geojson&access_token=${accessToken}`,
+        { method: 'GET' }
+      );
+      return await response.json();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  constructor() {
+    this.mapbox.accessToken = environment.mapBoxToken;
+  }
+
+  ngOnInit() {
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: this.zoom,
+      center: [this.lng, this.lat],
+    });
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.map.setCenter([position.coords.longitude, position.coords.latitude]);
+      new mapboxgl.Marker()
+        .setLngLat([position.coords.longitude, position.coords.latitude])
+        .addTo(this.map);
+    });
+    this.map.on('move', () => {
+      this.lng = parseFloat(this.map.getCenter().lng.toFixed(4));
+      this.lat = parseFloat(this.map.getCenter().lat.toFixed(4));
+      this.zoom = parseFloat(this.map.getZoom().toFixed(2));
+    });
+    let obstacle = buffer(this.recyclingPoints as any, 0.04, {
+      units: 'kilometers',
+    });
+    this.map.on('load', () => {
+      this.map.addLayer({
+        id: 'recyclingPoints',
+        type: 'fill',
+        source: {
+          type: 'geojson',
+          data: obstacle,
+        },
+        layout: {},
+        paint: {
+          'fill-color': '#f03b20',
+          'fill-opacity': 0,
+          'fill-outline-color': '#f03b20',
+        },
+      });
+    });
+    this.recyclingPoints.features.forEach((item) => {
+      const element = document.createElement('img');
+      element.src = '/assets/green-marker.svg';
+      new mapboxgl.Marker(element)
+        .setLngLat(item.geometry.coordinates as any)
+        .addTo(this.map);
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
+  mapBoxToken:
+    'pk.eyJ1IjoibWF0aWFndWlsZXJhIiwiYSI6ImNsZHVlOXFqZTA0emczbnA5eWxmaWNia2sifQ.G7L0t9gYA7XpsLuiEuVu1g',
   firebase: {
     projectId: 'puntos-de-reciclaje-88753',
     appId: '1:281014797212:web:83c0adadf42f01c34efe59',
